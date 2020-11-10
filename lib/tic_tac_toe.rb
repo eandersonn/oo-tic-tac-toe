008135c02a4560ff5d0aba6b96b36a1431061cd1
Erin Anderson <e.anderson1226@gmail.com>
2020-11-10 12:05:09 -0500

    commit tree 71b0e5061c8ebd51d4a7245e3674c8bf3c735773

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,51 @@
+class TicTacToe
+
+    WIN_COMBINATIONS = []
+
+    def initialize
+        @board = [" ", " ", " "," ", " ", " ", " ", " ", " ",]
+    end
+
+    def display_board
+        @board
+    end
+
+    def input_to_index 
+    end
+
+    def move
+    end
+
+    def position_taken
+    end
+
+    def valid_move
+    end
+
+    def turn
+    end
+
+    def turn_count
+    end
+
+    def current_player
+    end
+
+    def won
+    end
+
+    def full
+    end
+
+    def draw
+    end
+
+    def over
+    end
+    
+    def winner
+    end
+
+    def play
+    end
+end
